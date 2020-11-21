0814fae076d4fd6ce3cea78fd08e3f2d14c9df23
Vera <vera.borvinski@gmail.com>
2020-11-21 05:04:49 +0000

    works kinda now

--- unit_07/Activity1.js
+++ unit_07/Activity1.js
@@ -3,12 +3,30 @@ var bigbox = document.getElementById('bigbox');
 
 var colors = document.getElementsByClassName('row');
 
-var i;
-for (i=0; i<colors.length;i++){
- colors[i].addEventListener('mouseover', myFunction(){
-        bigbox.style.backgroundColor = window.getComputedStyle(this).backgroundColor;
-    });
-    colors[i].addEventListener('mouseleave', myFunction() {
-        bigbox.style.backgroundColor = "black";
-    });
-}
+document.getElementById("box1").addEventListener("mouseover", function() {
+  bigbox.style.backgroundColor = "tomato";
+});
+document.getElementById("box2").addEventListener("mouseover", function() {
+  bigbox.style.backgroundColor = "orange";
+});
+document.getElementById("box3").addEventListener("mouseover", function() {
+  bigbox.style.backgroundColor = "yellow";
+});
+document.getElementById("box4").addEventListener("mouseover", function() {
+  bigbox.style.backgroundColor = "lime";
+});
+document.getElementById("box5").addEventListener("mouseover", function() {
+  bigbox.style.backgroundColor = "green";
+});
+document.getElementById("box6").addEventListener("mouseover", function() {
+  bigbox.style.backgroundColor = "cyan";
+});
+document.getElementById("box7").addEventListener("mouseover", function() {
+  bigbox.style.backgroundColor = "turquoise";
+});
+document.getElementById("box8").addEventListener("mouseover", function() {
+  bigbox.style.backgroundColor = "blue";
+});
+document.getElementsByClassName("row").addEventListener("mouseout", function() {
+  bigbox.style.backgroundColor = "black";
+});
